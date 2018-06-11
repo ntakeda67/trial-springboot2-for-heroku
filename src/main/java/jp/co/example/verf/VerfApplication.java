e2cfb1f037f7aad3bc616d96dc6bbcf0123f39b0
nt67 <nt67@localhost.localdomain>
2018-06-12 01:10:50 +0900

    create via initializr

---
+++ src/main/java/jp/co/example/verf/VerfApplication.java
@@ -0,0 +1,12 @@
+package jp.co.example.verf;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class VerfApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(VerfApplication.class, args);
+	}
+}

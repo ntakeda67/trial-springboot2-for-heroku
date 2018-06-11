ecf604c26ada687e652c2a68af6c6a7f0066228d
ntakeda67 <n.takeda67@gmail.com>
2018-06-12 02:24:40 +0900

    sample simple controller

--- src/main/java/jp/co/example/verf/features/HomeController.java
+++ src/main/java/jp/co/example/verf/features/HomeController.java
@@ -1,7 +1,9 @@
 package jp.co.example.verf.features;
 
+import org.springframework.stereotype.Controller;
 import org.springframework.web.bind.annotation.*;
 
+@Controller
 public class HomeController {
 
     @RequestMapping("/")
